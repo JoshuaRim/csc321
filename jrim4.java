f555ba6f5cd1abc815328de09149e5a6030c39a5
JoshuaRim <105468483+JoshuaRim@users.noreply.github.com>
2025-02-04 18:28:51 -0800

    Update jrim4.java

--- jrim4.java
+++ jrim4.java
@@ -7,15 +7,31 @@ public class jrim4
 	{
 		int x = 0;
 		System.out.println("Choose 1 of the four options");
-		System.out.println("Yes");
-		System.out.println("No");
-		System.out.println("Maybe");
-		System.out.println("I don't know");
+		System.out.println("1. Yes");
+		System.out.println("2. No");
+		System.out.println("3. Maybe");
+		System.out.println("4. I don't know");
 
 		Scanner userinput = new Scanner(System.in);
 		x = userinput.nextInt();
 
-		System.out.println("You chose option "+x);
+		if (x == 1)
+		{
+			System.out.println("You chose 'Yes' - You do like school!")
+				}
+		else if (x == 2)
+		{
+			System.out.println("You chose 'No' - You do not like school!")
+				}
+		else if (x == 3)
+		{
+			System.out.println("You chose 'Maybe' - You kinda like school!")
+				}
+		else if (x == 4)
+		{
+			System.out.println)"You chose 'I don't know' - You dont know if you like or dislike school!")
+				}
+		
 	}
 }
 
